26a1858038b463d8a478983e87168ce9f3589036
Francois Brodeur <francois.brodeur@era.co>
2023-06-13 21:43:27 -0400

    Remove debug main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-use std::mem::size_of;
-
-use elab::Synth;
 use normalize::Norm;
 
 use crate::{
     elab::Elab,
     parse::parse_expr,
-    types::{Closure, Core, Loc, Pos, Value},
+    types::{Loc, Pos},
 };
 
 mod alpha_equiv;
@@ -16,23 +13,7 @@ mod normalize;
 mod parse;
 mod types;
 
-const SOURCE: &str = "<test input>";
-
-fn elab() -> Elab {
-    let loc = Loc {
-        source: "<test suite>".to_owned(),
-        start: Pos { line: 1, col: 1 },
-        end: Pos { line: 1, col: 1 },
-    };
-    Elab::new(loc)
-}
-
-fn norm() -> Norm {
-    Norm::default()
-}
-
 fn main() {
-    /*
     let expr = parse_expr("temp", "").unwrap();
     let loc = Loc {
         source: "<temp>".to_owned(),
@@ -42,17 +23,4 @@ fn main() {
     let synth = Elab::new(loc).synth(&expr).unwrap();
     dbg!(synth);
     println!("Hello, world!");
-    */
-    let input = "(which-Nat 40 't (lambda (x) 'nil))";
-    let input_expr = parse_expr(SOURCE, input).unwrap();
-    let Synth {
-        the_type: actual_ty,
-        the_expr: actual_core,
-    } = elab().synth(&input_expr).unwrap();
-    println!("Closure {}", size_of::<Closure<Value>>());
-    println!("Core {}", size_of::<Core>());
-    println!("Value {}", size_of::<Value>());
-    println!("Box value {}", size_of::<Box<Value>>());
-    let actual_value = norm().eval(&actual_core).unwrap();
-    dbg!(actual_value);
 }
